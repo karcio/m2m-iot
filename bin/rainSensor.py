cdb23fe5523b103d7f1c5dd4b3696d9d051a4b47
karcio <kczarnecki@hotmail.com>
2023-04-19 23:41:49 +0100

    [patch] added new sensors, ready to publish

---
+++ bin/rainSensor.py
@@ -0,0 +1,10 @@
+from time import sleep
+from gpiozero import InputDevice
+
+no_rain = InputDevice(7)
+
+while True:
+    if not no_rain.is_active:
+        print("It's raining - get the washing in!")
+    
+    sleep(1)
